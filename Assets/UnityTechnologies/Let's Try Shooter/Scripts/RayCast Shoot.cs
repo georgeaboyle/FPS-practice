af2062dc2b53ad63c681ad8d5faea2c624e74d15
georgeaboyle <georgeaboyle@gmail.com>
2024-02-28 14:52:29 +0200

    Checking in halfway through the project
    Added in the beginnings of RayCast creation. Haven't gotten to detection of hits, just where the laser will route to. About to add in the laser in the next step.
    
    At 20:57 in the video
    
    My headset died, so I can't listen to the information for a bit. Taking a break and will return to the video once it's charged up.

--- Assets/UnityTechnologies/Let's Try Shooter/Scripts/RayCast Shoot.cs
+++ Assets/UnityTechnologies/Let's Try Shooter/Scripts/RayCast Shoot.cs
@@ -29,6 +29,33 @@ public class RayCastShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+        }
+
+        StartCoroutine(ShotEffect());
+
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        laserLine.SetPosition(0, gunEnd.position);
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+        {
+            laserLine.SetPosition(1, hit.point);
+        }
+        else
+        {
+            laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+        }
+    }
+
+    private IEnumerator ShotEffect ()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
     }
 }
